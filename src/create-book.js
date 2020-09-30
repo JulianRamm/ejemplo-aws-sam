a3ba20a9873a02e1deb35c1ad772808aadabdce4
je.osorio <juliyanes530@gmail.com>
2020-09-30 12:24:09 -0500

    get y post

--- src/create-book.js
+++ src/create-book.js
@@ -1,7 +1,6 @@
 const AWS = require("aws-sdk");
 const crypto = require("crypto");
 
-// Generate unique id with no external dependencies
 const generateUUID = () => crypto.randomBytes(16).toString("hex");
 
 // Initialising the DynamoDB SDK
@@ -10,19 +9,18 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 exports.handler = async event => {
   const { title } = JSON.parse(event.body);
   const params = {
-    TableName: "books", // The name of your DynamoDB table
-    Item: { // Creating an Item with a unique id and with the passed title
+    TableName: "books",
+    Item: {
       id: generateUUID(),
       title: title
     }
   };
   try {
-    // Utilising the put method to insert an item into the table (https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.03.html#GettingStarted.NodeJs.03.01)
     const data = await documentClient.put(params).promise();
     const response = {
       statusCode: 200
     };
-    return response; // Returning a 200 if the item has been inserted 
+    return response;
   } catch (e) {
     return {
       statusCode: 500,
